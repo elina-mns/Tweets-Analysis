39331a6caa400fc36cac2bd8982456dce889be06
elina-mns <ellie.mansurova@gmail.com>
2021-01-05 18:01:39 -0500

    Initial Commit

---
+++ Tweets Analysis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tweets Analysis
+//
+//  Created by Elina Mansurova on 2021-01-05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
