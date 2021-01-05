8054e17ac90296161e0423b178c3909359a3e7f5
elina-mns <ellie.mansurova@gmail.com>
2021-01-05 18:12:07 -0500

    set up the view and constraints

--- Tweets Analysis/ViewController.swift
+++ Tweets Analysis/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var headerView: UIView!
+    var textField: UITextField!
+    var predictButton: UIButton!
+    var sentiment: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        headerView = UIView()
+        headerView.backgroundColor = .blue
+        self.view.addSubview(headerView)
+        
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
+        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15).isActive = true
+        
+        
     }
 
 
