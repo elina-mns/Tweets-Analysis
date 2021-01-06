080153837798de26d6297fb2506a4b4541d4e6c2
elina-mns <ellie.mansurova@gmail.com>
2021-01-05 19:00:49 -0500

    final set up of the view: add stackView, label, textField, button

--- Tweets Analysis/MainVC.swift
+++ Tweets Analysis/MainVC.swift
@@ -9,25 +9,53 @@ import UIKit
 
 class MainVC: UIViewController {
     
-    var headerView: UIView!
-    var textField: UITextField!
-    var predictButton: UIButton!
-    var sentiment: UILabel!
+    lazy var textField: UITextField = {
+        let textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.placeholder = "How do people feel about..."
+        textField.backgroundColor = .white
+        return textField
+    }()
+    lazy var predictButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .black
+        button.setTitle("PREDICT", for: .normal)
+        return button
+    }()
+    lazy var sentiment: UILabel = {
+        let label = UILabel()
+        label.text = "🤔"
+        label.font = UIFont.systemFont(ofSize: 70)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        return stackView
+    }()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        headerView = UIView()
-        headerView.backgroundColor = .purple
-        self.view.addSubview(headerView)
+        view.backgroundColor = .purple
+        view.addSubview(stackView)
         
-        headerView.translatesAutoresizingMaskIntoConstraints = false
-        headerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        headerView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        headerView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 1).isActive = true
-        headerView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
         
+        stackView.addArrangedSubview(sentiment)
+        stackView.addArrangedSubview(textField)
+        stackView.addArrangedSubview(predictButton)
         
+        sentiment.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.6).isActive = true
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        textField.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1).isActive = true
     }
 
 
